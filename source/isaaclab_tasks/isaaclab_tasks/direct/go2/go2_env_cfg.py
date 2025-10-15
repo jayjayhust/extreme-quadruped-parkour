e5c37e44fbfc30a3d1ded7dd5d0cf2d044432055
yobel-sungkooklee <yobellee@g.skku.edu>
2025-10-15 11:34:10 +0900

    Edit: Reward scale for Rough Terrain

--- source/isaaclab_tasks/isaaclab_tasks/direct/go2/go2_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/go2/go2_env_cfg.py
@@ -153,4 +153,7 @@ class Go2RoughEnvCfg(Go2FlatEnvCfg):
     )
 
     # reward scales (override from flat config)
-    flat_orientation_reward_scale = 0.0
+    base_height_reward_scale = -1.0 # Rough Terrain에서는 낮추는게 더 좋음(학습되는거 보고 -0.5로 바꿔야할수도).
+    flat_orientation_reward_scale = 0.0 # 험지니까 몸이 엄청 기울거라서
+    feet_air_time_reward_scale = 1.0 # (거친 지형에서는 발을 오래 떼려는 경향 크면 안 될듯-> 나중에 0.5까지로도 바꿔야 할수도)
+
