7a9c8ed0797c38e7d61111c6880da2fd27bf7f81
yobel-sungkooklee <yobellee@g.skku.edu>
2025-10-16 09:34:48 +0900

    Edit: Test2 Reward Scale Setting

--- source/isaaclab_tasks/isaaclab_tasks/direct/go2/go2_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/go2/go2_env_cfg.py
@@ -152,12 +152,12 @@ class Go2RoughEnvCfg(Go2FlatEnvCfg):
         mesh_prim_paths=["/World/ground"],
     )
 
-    # Test1 reward scales (override from flat config)
+    # Test2 reward scales (override from flat config)
     base_height_reward_scale = -2.0 # Rough Terrain에서는 낮추는게 더 좋음(학습되는거 보고 -1.0혹은 -0.5로 바꿔야할수도).
-    flat_orientation_reward_scale = 0.0 # 험지니까 몸이 엄청 기울거라서
-    feet_air_time_reward_scale = 1.0 # (거친 지형에서는 발을 오래 떼려는 경향 크면 안 될듯-> 나중에 0.5까지로도 바꿔야 할수도)
-    lin_vel_reward_scale = 3.0
-    yaw_rate_reward_scale = 1.5
+    flat_orientation_reward_scale = 0.0 # 험지니까 몸이 엄청 기울거라서    
+    feet_air_time_reward_scale = 0.5 # Test2에서는 더 작게
+    lin_vel_reward_scale = 5.0 # Test2에서는 더 크게
+    yaw_rate_reward_scale = 1.0 # Test2에서는 더 작게
     z_vel_reward_scale = -2.0
     ang_vel_reward_scale = -0.05
     joint_torque_reward_scale = -2.5e-5
