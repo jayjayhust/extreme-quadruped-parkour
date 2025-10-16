28bb3a04272348d5a33f7e3662c1d7a845eda440
yobel-sungkooklee <yobellee@g.skku.edu>
2025-10-16 09:32:09 +0900

    Edit: 아 헷갈릴 수 있는데, 지금은 이미 학습 다 된 앞의 commit에서 바꾼거 없이 Test1 reward scale이라고 이름만 명명

--- source/isaaclab_tasks/isaaclab_tasks/direct/go2/go2_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/go2/go2_env_cfg.py
@@ -152,7 +152,7 @@ class Go2RoughEnvCfg(Go2FlatEnvCfg):
         mesh_prim_paths=["/World/ground"],
     )
 
-    # reward scales (override from flat config)
+    # Test1 reward scales (override from flat config)
     base_height_reward_scale = -2.0 # Rough Terrain에서는 낮추는게 더 좋음(학습되는거 보고 -1.0혹은 -0.5로 바꿔야할수도).
     flat_orientation_reward_scale = 0.0 # 험지니까 몸이 엄청 기울거라서
     feet_air_time_reward_scale = 1.0 # (거친 지형에서는 발을 오래 떼려는 경향 크면 안 될듯-> 나중에 0.5까지로도 바꿔야 할수도)
